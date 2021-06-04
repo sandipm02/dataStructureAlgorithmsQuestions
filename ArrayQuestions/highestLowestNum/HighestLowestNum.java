a02598105c0e739c43f922c9e11a64435a2d4e83
Sandip Mishra <sandipm02@icloud.com>
2021-06-03 22:14:36 -0600

    started highest lowest

---
+++ ArrayQuestions/highestLowestNum/HighestLowestNum.java
@@ -0,0 +1,28 @@
+/**
+ * Question: 
+ * How do you find the largest and smallest number in an unsorted integer array?
+ */
+
+class HighestLowestNum {
+    public static void main(String[] args) {
+        Setup set = new Setup();
+        solution(); //insert required paramenters to finish solution
+    }
+
+/**
+ * Answer:
+ * #### insert explanation
+ * 
+ */
+    public static void solution() {
+
+        }
+}
+
+/**
+ * This class is used to setup the problem
+ */
+
+class Setup {
+
+}
